c9b4d007726cb2067d4e342dea05dbbd6aa43c98
Thomas Dekiere <t.dekiere@gmail.com>
2024-12-20 14:49:45 +0100

    feat: make lib-c depend on lib-b

--- libs/lib-c/src/lib/lib-c.ts
+++ libs/lib-c/src/lib/lib-c.ts
@@ -1,5 +1,6 @@
 import { libA } from '@thdk/lib-a';
+import { libB } from '@thdk/lib-b';
 
 export function libC(): string {
-  return `lib-c depends on ${libA()}`;
+  return `lib-c depends on ${libA()} and ${libB()}`;
 }
